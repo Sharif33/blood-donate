4481670f648a85d6a957c75848f4f7ebf38c9a9b
Sharif33 <sharif.cse.nu@gmail.cm>
2022-01-02 21:51:57 +0600

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import './App.css';
+
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './Components/Pages/Home/Home';
 import ContactUs from './Components/Pages/Home/ContactUs';
 import AboutUs from './Components/Pages/Home/AboutUs';
 import HeaderTop from './Components/Shared/Header/HeaderTop';
+import AuthProvider from './Components/Contexts/AuthProvider/AuthProvider';
 
 function App() {
   return (
     <div>
       <HeaderTop/>
+      <AuthProvider>
       <Routes>
-        
          <Route path="/" element={<Home />} />
           <Route path="home" element={<Home />} />
           <Route path="contact" element={<ContactUs />} />
           <Route path="about" element={<AboutUs />} />
       </Routes>
+      </AuthProvider>
      
     </div>
   );
